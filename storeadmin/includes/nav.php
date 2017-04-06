e2cad44d272ce574bf4787f0641ec4bbfd64b052
vikas meena <vikasmeena0418@gmail.com>
2017-04-07 02:47:46 +0530

    Add files via upload

---
+++ storeadmin/includes/nav.php
@@ -0,0 +1,44 @@
+<div class="header">
+	<div class="header-top">
+		<div class="container">
+			<div class="search">
+					<form>
+						<input type="text" value="Search " onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+						<input type="submit" value="Go">
+					</form>
+			</div>
+			<div class="header-left">		
+					<ul>
+						<li ><a href="login.php"  >Login</a></li>
+						<li><a  href="register.php"  >Register</a></li>
+					</ul>
+					<div class="cart box_1">
+						<a href="checkout.php">
+						<h3> <div class="total">
+							<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</div>
+							<img src="../images/cart.png" alt=""/></h3>
+						</a>
+					</div>
+					<div class="clearfix"> </div>
+			</div>
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+		<div class="container">
+			<div class="head-top">
+				<div class="logo">
+					<a href="index.php"><img src="../images/logo.png" alt="" width="150px"></a>	
+				</div>
+		  <div class=" h_menu4">
+				<ul class="memenu skyblue">
+				<li class="active grid"><a class="color8" href="brands.php">Brands</a></li>
+				<li><a class="color4" href="products.php">Products</a></li>				
+				<li><a class="color6" href="contact.php">Contact</a></li>
+			  </ul> 
+			</div>
+				
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+
+	</div>
