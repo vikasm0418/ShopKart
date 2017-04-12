7ce4cb47b272e6deea833e833558db4f197f4e59
vikas meena <vikasmeena0418@gmail.com>
2017-04-12 05:47:16 +0530

    updated categories.php

--- storeadmin/categories.php
+++ storeadmin/categories.php
@@ -6,9 +6,18 @@ require $_SERVER['DOCUMENT_ROOT'].'/ShopKart/storescripts/connect_to_mysql.php';
 
 		$sql = "SELECT * FROM categories WHERE parent = 0";
 		$result = mysqli_query($con,$sql);
-		 $errors  = array();
+		$errors  = array();
+		$category = '';
+		$post_parent='';
 		 //edit branch
+		if(isset($_GET['edit']) && !empty($_GET['edit'])){
+	  	$edit_id = (int)$_GET['edit'];
+	  	$edit_id = sanitize($edit_id);
 
+	  	$sql4 = "SELECT * FROM categories WHERE id = '$edit_id'";
+	  	$result4 = mysqli_query($con,$sql4);
+	  	$category = mysqli_fetch_assoc($result4);
+	  	}
 		 //delete categories
 		if(isset($_GET['delete']) && !empty($_GET['delete'])){
 		$delete_id = (int)$_GET['delete'];
@@ -28,9 +37,13 @@ require $_SERVER['DOCUMENT_ROOT'].'/ShopKart/storescripts/connect_to_mysql.php';
 
 		//process form
 		if(isset($_POST) && !empty($_POST)){;
-	  	$parent = sanitize($_POST['parent']);
+	  	$post_parent = sanitize($_POST['parent']);
 	  	$category = sanitize($_POST['category']);
-	  	$sql3 = "SELECT * FROM categories WHERE category = '$category' AND parent='$parent'";
+	  	$sql3 = "SELECT * FROM categories WHERE category = '$category' AND parent='$post_parent'";
+		if(isset($_GET['edit'])){
+			$id = $category1['id'];
+			$sql3 = "SELECT * FROM categories WHERE category = '$category' AND parent='$post_parent' AND id!='$id'";
+		}
 		$result3 = mysqli_query($con,$sql3);
 		$count = mysqli_num_rows($result3);
 
@@ -52,9 +65,23 @@ require $_SERVER['DOCUMENT_ROOT'].'/ShopKart/storescripts/connect_to_mysql.php';
 	  <?php }else
 	  {
 	  	//update database
-	  	$sql5 = "INSERT INTO categories (category,parent) VALUES ('$category','$parent')";
+	  	$sql5 = "INSERT INTO categories (category,parent) VALUES ('$category','$post_parent')";
+	  	if(isset($_GET['edit'])){
+	  		$sql5 = "UPDATE categories SET category='$category' ,parent='$post_parent' WHERE id='$edit_id'";
+	  	}
 		$result5 = mysqli_query($con,$sql5);
 		header('Location:categories.php');
+	  }
+	  }
+	  $category_value = '';
+	  $parent_value = '0';
+	  if(isset($_GET['edit'])){
+	  	$category_value = $category['category'];
+	  	$parent_value = $category['parent'];
+	  }else{
+	  	if(isset($_POST['edit'])){
+	  	$category_value = $category;
+	  	$parent_value = $post_parent;
 	  }
 	  }
 ?>
@@ -62,22 +89,22 @@ require $_SERVER['DOCUMENT_ROOT'].'/ShopKart/storescripts/connect_to_mysql.php';
 <div class="row">
 	<div class="col-md-6">
 	<!--form -->
-	<form class="form" action="categories.php" method="post">
+	<form class="form" action="categories.php<?php echo ((isset($_GET['edit']))?'?edit='.$edit_id:''); ?>" method="post">
 		<div class="form-group">
-			<legend>Add a Category</legend>
+			<legend><?=((isset($_GET['edit']))?'Edit a ':'Add a ');?>Category</legend>
 			<div id="errors"></div>
 			<label for="parent">Parent</label>
 			<select class="form-control" name="parent" id ="parent">
-				<option value="0">Parent</option>
+				<option value="0" <?=(($parent_value == 0)?'selected="selected"':'');?>>Parent</option>
 				<?php while($parent = mysqli_fetch_assoc($result)): ?>
-					<option value="<?=$parent['id'];?>"><?= $parent['category']; ?></option>
+					<option value="<?=$parent['id'];?>" <?=(($parent_value == $parent['id'])?'selected="selected"':'');?>><?= $parent['category']; ?></option>
 				<?php endwhile; ?>
 			</select>
 		<div class="form-group">
 			<label for="category">Category</label>
-			<input type="text" class="form-control" name="category" id ="category">
+			<input type="text" class="form-control" name="category" value="<?= $category_value ?>" id ="category">
 		</div>
-			<input type="submit" name="add_submit" value="Add category" class="btn btn-lg btn-success">
+			<input type="submit" name="add_submit" value="<?=((isset($_GET['edit']))?'Edit a ':'Add a ');?>category" class="btn btn-lg btn-success">
 		</div>
 	</form><hr><br>
 	</div>
