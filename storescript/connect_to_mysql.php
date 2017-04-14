1610d5666ea2dacd7efb3c9e260030da0ff7b3e7
vikas meena <vikasmeena0418@gmail.com>
2017-04-15 05:03:17 +0530

    connect_to_mysql.php updated

--- storescript/connect_to_mysql.php
+++ storescript/connect_to_mysql.php
@@ -6,6 +6,29 @@ $db_name="ShopKart";
 
 $con = mysqli_connect($db_host,$db_username,$db_pass,$db_name) or die("could not connect to mysql");
 mysqli_select_db($con,$db_name) or die ("no databse");
-require '../config.php';
-require '../helpers/helpers.php';
+
+session_start();
+
+require $_SERVER['DOCUMENT_ROOT'].'/ShopKart/config.php';
+require BASEURL.'/helpers/helpers.php';
+
+if(isset($_SESSION['SBUser'])){
+	$userID = $_SESSION['SBUser'];
+	$sql8 = "SELECT * FROM users WHERE id='$userID'";
+	$result8 = mysqli_query($con,$sql8);
+	$user_data = mysqli_fetch_assoc($result8);
+	$fn = explode(' ',$user_data['full_name']);
+	$user_data['first'] = $fn[0];
+	$user_data['last'] = $fn[1];
+}
+
+if(isset($_SESSION['success_flash'])){
+	echo '<div class="bg-success"><p class="text-success text-center">'.$_SESSION['success_flash'].'</p></div>';
+	unset($_SESSION['success_flash']);
+}
+if(isset($_SESSION['error_flash'])){
+	echo '<div class="bg-danger"><p class="text-danger text-center">'.$_SESSION['error_flash'].'</p></div>';
+	unset($_SESSION['error_flash']);
+}
+
 ?>
